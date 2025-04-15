68588ae0259271f7f49cc8af40aacad2afdb3a3f
VictorHxHugo <victorzk6@hotmail.com>
2025-04-15 01:54:53 -0300

    Styled Component Props and the "css" Function

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -56,8 +56,11 @@ const Button = styled.button`
   border-radius: var(--border-radius-sm);
   background-color: var(--color-brand-600);
   color: var(--color-brand--50);
-  box-shadow: var() (--shadow-sm);
+  box-shadow: var(--shadow-sm);
   cursor: pointer;
+
+  ${(props) => sizes[props.size]}
+  ${(props) => variations[props.variation]}
 
   &:hover {
     background-color: var(--color-brand-700);
